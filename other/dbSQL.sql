441e797ceb3202c132b1207924f01f9635becd8f
RobynJiang <robynjiang1024@gmail.com>
2024-10-29 11:22:10 -0700

    Add expected query result

--- other/dbSQL.sql
+++ other/dbSQL.sql
@@ -1,8 +1,12 @@
 -- How many students have registered for the upcoming career fair？
 SELECT COUNT(user_id) as Students_num
-FROM Student s
-JOIN Register_CareerFair r ON s.user_id = r.user_id
-Where r.fair_id = 1; -- Adjust fair_id based on the upcoming fair
+FROM Students
+NATURAL JOIN Register_CareerFair
+Where fair_id = 1; -- Adjust fair_id based on the upcoming fair
+/*
+Students_num
+2
+*/
 
 -- Which companies are offering internship positions during the fair?
 SELECT DISTINCT c.company_name
@@ -10,8 +14,12 @@ FROM Companies c
 JOIN Position p ON c.user_id = p.company_id
 JOIN Attend_careerFair a ON c.user_id = a.user_id
 WHERE p.intern_flag = TRUE
-  AND a.fair_id = 1  -- Adjust fair_id based on the fair
+  AND a.fair_id = 4  -- Adjust fair_id based on the fair
   AND a.status = 'confirmed';
+  /*
+  company_name
+  FinanceInc
+  */
 
 -- Which company has listed the most job positions in the career fair?
 WITH CompanyPositions AS (
@@ -19,7 +27,7 @@ WITH CompanyPositions AS (
     FROM Companies c
     JOIN Position p ON c.user_id = p.company_id
     JOIN Attend_careerFair a ON c.user_id = a.user_id
-    WHERE a.fair_id = 1  -- Adjust fair_id based on the fair
+    WHERE a.fair_id = 5  -- Adjust fair_id based on the fair
       AND a.status = 'confirmed'
     GROUP BY c.company_name
 )
@@ -27,17 +35,33 @@ SELECT company_name, Num_positions
 FROM CompanyPositions cp
 ORDER BY Num_positions DESC
 LIMIT 1;
+/*
+company_name  Num_positions
+FinaneInc	  3
+*/
 
 -- What is the registration count per major for students?
 SELECT s.major, COUNT(r.user_id) AS registered_count_per_major
 FROM Students s
 JOIN Register_CareerFair r ON s.user_id = r.user_id
 GROUP BY s.major;
+/*
+major					registered_count_per_major
+Computer Science		1
+Information Systems 	1
+Data Science	    	1
+Cybersecurity			1
+Business Analytics  	1
+*/
 
 -- How many registered students have attended for the upcoming fair?
 SELECT COUNT(DISTINCT a.user_id) AS attended_students_count
 FROM Attend_careerFair a
 JOIN Register_CareerFair r ON a.fair_id = r.fair_id AND a.user_id = r.user_id
 WHERE a.fair_id = 1; -- Adjust fair_id based on the upcoming fair
+/*
+attend_students_count
+2
+*/
 
 
